1673df71196916f69acc02af01e1e051f851a93e
Chance <chanceusc@gmail.com>
2022-02-13 11:01:48 -0500

    fixes formatting

--- protoc-gen-echo/src/main.rs
+++ protoc-gen-echo/src/main.rs
@@ -2,9 +2,9 @@ use std::io::{self, Read, Write};
 fn main() {
     let mut buf: Vec<u8> = Vec::default();
     io::stdin().read_to_end(&mut buf).unwrap();
-    eprintln!("[echo start]");
     let stderr = io::stderr();
     let mut handle = stderr.lock();
+    handle.write_all(b"[echo start]\n").unwrap();
     handle.write_all(&buf).unwrap();
-    eprintln!("[echo end]");
+    handle.write_all(b"\n[echo end]").unwrap();
 }
